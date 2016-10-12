ed9e6f8563008dfb6572afb3ac6b51a39358bd85
Ajmal Hussain HY <AsathalMannan@users.noreply.github.com>
2016-10-12 09:37:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
 console.log('Loaded!');
 
-var element = document.getElementById("new");
-element.innerHtml = 'Client javascript is running..!';
+var element = document.getElementById('new');
+element.innerHTML = 'Client javascript is running..!';
