ee2cb3b4a71c9ff64cf1201ce508f0326a226396
AsathalMannan <ajmalhussainhy@gmail.com>
2016-10-12 11:58:41 +0530

    xml request added

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,18 @@
-console.log('Loaded!');
+//Counter code
+var button = document.getElementById('counter');
+
+button.onclick = function() {
+    //make a request and store it in a variable
+    var request = new XMLHttpRequest();
+    //capture the response and store it in a variable
+    request.onreadystatechange = function() {
+    if(request.readyState === XMLHttpRequest.DONE){
+        if(request.status === 200){
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+        //not necessary
+    }
+    }
+}
