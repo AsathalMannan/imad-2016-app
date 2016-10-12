731fc72f458b7ce6ff6688b7c4ebe4aedc2843f4
Ajmal Hussain HY <AsathalMannan@users.noreply.github.com>
2016-10-12 09:44:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 
 var element = document.getElementById('new');
 element.innerHTML = 'Client javascript is running..!';
+
+var img = document.getElementById('madi');
+img.onclick = function() {
+    img.style.marginLeft = '100px';
+}
