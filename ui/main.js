5cb67ad17eae98ebb50f07d4f7854dcbfe249619
Ajmal Hussain HY <AsathalMannan@users.noreply.github.com>
2016-10-12 10:13:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,1 @@
 console.log('Loaded!');
-
-var element = document.getElementById('new');
-element.innerHTML = 'Client javascript is running..!';
-
-var img = document.getElementById('madi');
-var moveleft = 0;
-function moveRight(){
-    moveleft = moveleft+'1';
-    img.style.marginLeft = moveleft + 'px';
-}
-img.onclick = function() {
-    var interval =  setInterval(moveRight, 100);
-}
